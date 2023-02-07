1573e2633c872de9a969f6aa6555f3ea9854e4c4
Alexander Fritsch <alexander.fritsch@adtran.com>
2023-02-07 13:09:19 +0100

    added check_wcs_aros_x86_64.sh (does still nothing)

---
+++ Amiga/check_wcs_aros_x86_64.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+  
+#AF, HGW, 9.Dec.2022
+# Perform some checks on the compiled WCS program
+# Parameter: executable name
+
+
+#checks for Aros x86_64 version of WCS
+
+echo "Currently no tests for Aros x86_64 version of WCS performed."
