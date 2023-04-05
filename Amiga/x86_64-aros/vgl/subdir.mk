7adee63e7d9ab13d7d9624e4dfca286163c1e4c5
Alexander Fritsch <alexander.fritsch@adtran.com>
2023-04-05 18:00:03 +0200

    AROS64: restored compiler settings

--- Amiga/x86_64-aros/vgl/subdir.mk
+++ Amiga/x86_64-aros/vgl/subdir.mk
@@ -41,7 +41,7 @@ OBJS += \
 vgl/%.o: ../vgl/%.c vgl/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	x86_64-aros-gcc -DFORCE_MUIMASTER_VMIN=19 -D__far= -DAMIGA_GUI -DTOOLCHAIN_VER=\"'$(shell m68k-amigaos-toolchain_hashes.sh | tr '!-~' 'P-~!-O' | sed 's/\\/\\\\/g' )'\" -DUSHORT=UWORD -DSHORT=int16_t -D__stdargs= -D__chip= -D__saveds= -DSTACKED= -DMUI_OBSOLETE -I"/home/developer/Desktop/SelcoGit/3DNature/Amiga" -O0 -g3 -Wall -c -fmessage-length=0 -funsigned-char -fno-common -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<" -DBUILDID=\"g/'$(shell git describe --always --dirty --exclude "*")'\" -DSTATIC_FCN=static -DSTATIC_VAR=static -Winline -DSWMEM_FAST_INLINE -g
+	x86_64-aros-gcc -DFORCE_MUIMASTER_VMIN=19 -D__far= -DAMIGA_GUI -DTOOLCHAIN_VER=\"'$(shell m68k-amigaos-toolchain_hashes.sh | tr '!-~' 'P-~!-O' | sed 's/\\/\\\\/g' )'\" -DUSHORT=UWORD -DSHORT=int16_t -D__stdargs= -D__chip= -D__saveds= -DSTACKED= -DMUI_OBSOLETE -I"/home/developer/Desktop/SelcoGit/3DNature/Amiga" -O3 -Wall -c -fmessage-length=0 -funsigned-char -fno-common -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<" -DBUILDID=\"g/'$(shell git describe --always --dirty --exclude "*")'\" -DSTATIC_FCN=static -DSTATIC_VAR=static -Winline -DSWMEM_FAST_INLINE -g
 	@echo 'Finished building: $<'
 	@echo ' '
 
