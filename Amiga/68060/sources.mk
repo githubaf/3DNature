78ba1017fcf543f45f2b92740127a7d959366a66
Alexander Fritsch <alexander.fritsch@adtran.com>
2025-02-10 12:42:01 +0100

    work an pre-commit and coverage. Did/does(?) not work on af-hp... 21.5 percent now

--- Amiga/68060/sources.mk
+++ Amiga/68060/sources.mk
@@ -15,7 +15,5 @@ OBJS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 . \
-Coverage \
-Coverage/vgl \
 vgl \
 
--- Amiga/test_i386-aros/sources.mk
+++ Amiga/test_i386-aros/sources.mk
@@ -15,6 +15,4 @@ OBJS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 . \
-Coverage \
-Coverage/vgl \
 
