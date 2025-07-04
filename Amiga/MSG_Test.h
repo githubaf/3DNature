34b0916a3623ff361a49e22b47a3fde2e03dfdff
Alexander Fritsch <alexander.fritsch@adtran.com>
2025-07-04 16:08:18 +0200

    moved Msg-Test to a new c-file

---
+++ Amiga/MSG_Test.h
@@ -0,0 +1,16 @@
+/*
+ * MSG_Test.h
+ *
+ *  Created on: Jul 4, 2025
+ *      Author: afritsch
+ */
+
+#ifndef MSG_TEST_H_
+#define MSG_TEST_H_
+
+// #############################################
+void IncAndShowTestNumbers(unsigned int TestNumber, unsigned int TotalNumber);
+
+void Test_User_Message(unsigned int StartTestNumber);
+
+#endif /* MSG_TEST_H_ */
